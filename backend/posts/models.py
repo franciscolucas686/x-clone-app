7ce4ea0ffece3eab9cd0562580cf47fa470e0019
Francisco Lucas <franciscolucas686@gmail.com>
2025-08-02 06:18:35 -0300

    :sparkles: feat(views): create views for like, post and comment

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -6,3 +6,21 @@ User = settings.AUTH_USER_MODEL
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post de {self.user.username}"
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
