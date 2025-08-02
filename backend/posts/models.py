fe42adecc1c64209d03c90fa1d2131e2afdbec67
Francisco Lucas <franciscolucas686@gmail.com>
2025-08-01 21:49:42 -0300

    :sparkles: feat(posts): add initial post app structure

---
+++ backend/posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    text = models.TextField(blank=True)
