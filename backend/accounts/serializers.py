5e916ffa9f4b132dc86f16bc574d9df8b649a082
Francisco Lucas <franciscolucas686@gmail.com>
2025-08-14 06:18:24 -0300

    :lipstick: feat: create avatar-image

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -4,17 +4,24 @@ from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
+    avatar = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'avatar']
 
     def create(self, validated_data):
+        avatar = validated_data.pop('avatar', None)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+
+        if avatar:
+            user.avatar = avatar
+            user.save()
+        
         return user
     
 class UserProfileSerializer(serializers.ModelSerializer):
