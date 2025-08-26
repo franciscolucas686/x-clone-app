0d10cc734010bfd83feda50befec19ebe9ad7383
Francisco Lucas <franciscolucas686@gmail.com>
2025-08-25 21:26:14 -0300

    :wastebasket: test: create test avatar image and update

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -4,13 +4,20 @@ from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True)
     avatar = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'avatar']
+        fields = ['id', 'username', 'email', 'password','confirm_password', 'avatar']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({"confirm_password": "As senhas não são iguais."})
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password')
         avatar = validated_data.pop('avatar', None)
         user = User.objects.create_user(
             username=validated_data['username'],
@@ -23,6 +30,11 @@ class RegisterSerializer(serializers.ModelSerializer):
             user.save()
         
         return user
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Este email já está em uso.")
+        return value
     
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
