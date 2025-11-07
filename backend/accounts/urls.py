1b770b0957107b5775543c12d8a8bab25691b772
Francisco Lucas <franciscolucas686@gmail.com>
2025-11-07 05:26:51 -0300

    :recycle: refactor: exclude / from urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('/users/', UserListView.as_view(), name='user-list'),
+    path('users/', UserListView.as_view(), name='user-list'),
 ]
