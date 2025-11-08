1f9e2c3c2ec3af448079f0790af7779405a48d4d
Francisco Lucas <franciscolucas686@gmail.com>
2025-11-08 16:48:59 -0300

    :sparkles: feat: add following and followers feat and context

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -14,13 +14,28 @@ class PostSerializer(serializers.ModelSerializer):
     likes_count = serializers.SerializerMethodField()
     comments_count = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'user', 'text', 'created_at', 'comments', 'likes_count', 'comments_count']
+        fields = [
+            'id',
+            'user',
+            'text', 'created_at',
+            'comments',
+            'likes_count',
+            'comments_count',
+            'is_liked',
+        ]
 
     def get_likes_count(self, obj):
         return obj.likes.count()
     
     def get_comments_count(self, obj):
         return obj.comments.count()
+    
+    def get_is_liked(self, obj):
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return Like.objects.filter(post=obj, user=request.user).exists()
+        return False
