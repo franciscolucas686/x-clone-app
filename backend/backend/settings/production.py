abb8eb8056d45617d5e6e71ca4e21abdbf5d986f
Francisco Lucas <franciscolucas686@gmail.com>
2025-11-20 05:18:52 -0300

    :bug: fix(storage): correct Cloudinary initialization and media URL handling

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -35,6 +35,7 @@ CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    "CLOUDINARY_URL": os.getenv("CLOUDINARY_URL"),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
@@ -43,8 +44,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
-
-MEDIA_URL = ""
 
 CORS_ALLOW_ALL_ORIGINS = False
 
